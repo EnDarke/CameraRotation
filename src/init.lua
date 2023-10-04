2816146da91972a1912c39d99af6f26afb90b1b1
Alex Normann <normpup02@gmail.com>
2023-10-04 10:29:40 -0500

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -5,33 +5,20 @@
 
 --\\ Services //--
 local RunService = game:GetService("RunService")
-
---\\ Variables //--
 local vector3 = Vector3.new
-
-local pi = math.pi
+local cos = math.cos
+local sin = math.sin
 
 --\\ Local Utility Function //--
-local function calcRingPosition(args: {}, offset)
-    -- Prohibit continuation without necessary information.
-    if not ( args ) then return end
-
-    -- Prep variables
-    local _time = args.time
-    local distance = args.distance
-    local speed = args.speed
-    local height = args.height
-
-    if not ( _time and distance and speed ) then return end
-
-    return offset + vector3(math.cos(_time * speed) * distance, height, math.sin(_time * speed) * distance)
+local function calcRingPosition(time, distance, speed, height, offset)
+    return offset + vector3(cos(time * speed) * distance, height, sin(time * speed) * distance)
 end
 
 --\\ Module Code //--
 local CameraRotation = {}
 CameraRotation.__index = CameraRotation
 
-function CameraRotation.new(camera: Camera, instance: Instance, args: {})
+function CameraRotation.new(camera, instance, args)
     local self = setmetatable({}, CameraRotation)
 
     -- Initiates Camera Rotation Data
@@ -48,11 +35,9 @@ function CameraRotation.new(camera: Camera, instance: Instance, args: {})
     return self
 end
 
-function CameraRotation:ChangeObject(args: {})
-    -- Prohibit continuation without necessary information
-    if not ( self.onRenderStep ) then return end
+function CameraRotation:ChangeObject(args)
+    if not self.onRenderStep then return end
 
-    -- End previous rotation
     self:End()
 
     -- Change looking instance
@@ -61,50 +46,38 @@ function CameraRotation:ChangeObject(args: {})
     self.speed = args.Speed or self.speed
     self.height = args.Height or self.height
 
-    -- Start new rotation
     self:Start()
-
-    return true
 end
 
 function CameraRotation:Start()
-    -- Prohibit continuation without necessary information.
-    if ( self.onRenderStep ) then return end
-    if not ( self.distance and self.speed ) then return end
+    if self.onRenderStep then return end
 
-    -- Initiate Start
     local _time = 0
-
     local offset = self.instance.CFrame
     local distance = self.distance
     local speed = self.speed
     local height = self.height
 
-    -- RenderStep Event
     self.onRenderStep = RunService.RenderStepped:Connect(function(deltaTime)
-        _time += deltaTime
-        self.camera.CFrame = calcRingPosition({ time = _time, distance = distance, speed = speed, height = height }, offset)
+        _time = _time + deltaTime
+        self.camera.CFrame = calcRingPosition(_time, distance, speed, height, offset)
         self.camera.CFrame = CFrame.lookAt(self.camera.CFrame.Position, self.instance.Position)
     end)
 end
 
 function CameraRotation:End()
-    -- Prohibit continuation without necessary information.
-    if not ( self.onRenderStep ) then return end
+    if not self.onRenderStep then return end
 
     self.onRenderStep:Disconnect()
     self.onRenderStep = nil
-
-    return true
 end
 
 function CameraRotation:Destroy()
-    -- End Rotation Instance
     self:End()
-
-    -- Clear metatables
     setmetatable(self, nil)
-    table.clear(self)
+    for k in pairs(self) do
+        self[k] = nil
+    end
 end
 
 return CameraRotation
